135da992d16df7d8c28d3324c0f3502ae25758ad
Abhi-10949 <23bcs10949@cuchd.in>
2024-11-27 14:58:21 +0530

    changes

---
+++ Java_tutorial/First.java
@@ -0,0 +1,7 @@
+package JavaPlayground.JavaTutorial; // Corrected package name (removed hyphen and changed to camel case)
+
+public class First { // Changed class name to a valid identifier
+    public static void main(String[] args) { // Standardized the main method parameter
+        System.out.println("Hello World");
+    }
+}

0312093bb2cb5c69e3eb0936bc8f61ae71b20539
Abhi-10949 <23bcs10949@cuchd.in>
2024-11-27 14:39:39 +0530

    Removed the older folder

--- Java/twentyeight.java
+++
@@ -1,7 +0,0 @@
-public class twentyeight {
-    public static void main(String[] args){
-        //remove the extra spaces from the string
-        String str="       aBhishek       Kumar     ";
-        System.out.println(str.replaceAll("\\s+", " ").trim());
-    }
-}
--- Java/twentyfour.java
+++
@@ -1,9 +0,0 @@
-public class twentyfour{
-    public static void main(String[] args){
-        //checks wether the number is binary or not
-        int i =1001010;
-        String str=String.valueOf(i);
-        System.out.println(str.matches("[01]+"));
-
-    }
-}

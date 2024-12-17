e579808728585cd35029cf77e2cb34698ea9e20d
Abhi-10949 <23bcs10949@cuchd.in>
2024-12-17 13:33:44 +0530

    New folder added

---
+++ J3_Variables.java
@@ -0,0 +1,16 @@
+
+
+public class J3_Variables {
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name= "Abhishek Kumar!";
+        System.out.println(name);  //we can print any Datatype of variable.
+
+        a=b;  //we can change the value of the variable.
+        System.out.println(a);
+    }
+}
---
+++ J4_DataTypes.java
@@ -0,0 +1,23 @@
+
+
+public class J4_DataTypes {
+    public static void main(String args[]){
+        short a=20;
+        byte b=10;  //range 0-256
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var =true;
+        float price =10.5f;
+        System.out.println(price);
+        int number=25;
+        //long
+        //double
+        short n= 240;
+        System.out.println(var);
+
+        int sum;
+        sum = a+b;
+        System.out.println(sum);
+    }    
+}

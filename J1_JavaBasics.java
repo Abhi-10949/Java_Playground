286a32ca19fd9b7fcc16cd022b375bfdeebfc92f
Abhi-10949 <23bcs10949@cuchd.in>
2024-12-17 12:44:18 +0530

    First change

---
+++ J1_JavaBasics.java
@@ -0,0 +1,6 @@
+public class J1_JavaBasics {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.println("This is my first Java Program.");
+    }    
+}

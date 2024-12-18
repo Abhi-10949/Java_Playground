6bec4fc8b89066c725f3662eb387c487281f0488
Abhi-10949 <23bcs10949@cuchd.in>
2024-12-18 23:21:17 +0530

    updated

--- J33_Pattern4.java
+++ J33_Pattern4.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class J33_Pattern4 {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        char ch ='A';
+        for(int i=1;i<=n;i++){
+            for(int chars=1;chars<=i;chars++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
 }

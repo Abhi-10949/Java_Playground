2c59fb501872ed2d6ddb63b0ee7b5fec4d1a2863
Abhi-10949 <23bcs10949@cuchd.in>
2024-12-22 10:42:01 +0530

    new file

---
+++ J46_Pattern5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class J46_Pattern5 {
+    //Hollow rectangle
+    public static void hollow_rectangle(int totalRows, int totalCols){
+        for(int i =0 ; i <= totalRows ; i++){
+            for(int j = 0 ; j <= totalCols ; j++){
+                if(i == 0 || i == totalRows || j == 0 || j == totalCols){
+                    System.out.print("$");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("\nEnter the number of columns: ");
+        int cols = sc.nextInt();
+        hollow_rectangle(rows, cols);
+    }
+}

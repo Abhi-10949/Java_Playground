ecd5ec25d95e366bb1aef383e15aa8148cbca5e4
Abhi-10949 <23bcs10949@cuchd.in>
2025-01-29 22:48:51 +0530

    new file

--- J88_stringCompression.java
+++ J88_stringCompression.java
@@ -1,20 +1,36 @@
 public class J88_stringCompression {
 
     public static String compress(String str){
-        String newString = "";
+        // String newString = "";
 
-        for(int i = 0 ;i<str.length();i++){
-            Integer count = 1; 
-            while(i<str.length()-1 && str.charAt(i)== str.charAt(i+1)){
+        // for(int i = 0 ;i<str.length();i++){
+        //     Integer count = 1; 
+        //     while(i<str.length()-1 && str.charAt(i)== str.charAt(i+1)){
+        //         count++;
+        //         i++;
+        //     }
+        //     newString += str.charAt(i);
+        //     if(count >1){
+        //         newString += count.toString();
+        //     }
+        // }
+        // return newString;
+
+        StringBuilder newString = new StringBuilder("");
+
+        for(int i =0;i<str.length();i++){
+            int count = 1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
                 count++;
                 i++;
             }
-            newString += str.charAt(i);
-            if(count >1){
-                newString += count.toString();
+            newString.append(str.charAt(i));
+            if(count>1){
+                newString.append(count);
             }
         }
-        return newString;
+        return newString.toString();
+
     }
 
     public static void main(String[] args) {

4ef7001322ae5c088199e0c5d83cdf36c20b7f06
Abhi-10949 <23bcs10949@cuchd.in>
2025-07-18 16:42:39 +0530

    modified

--- J38_IsPrime.java
+++ J38_IsPrime.java
@@ -14,6 +14,7 @@ public class J38_IsPrime {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
         int n=sc.nextInt();
         System.out.println(isPrime(n));
     }
